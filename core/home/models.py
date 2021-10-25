727937af8cb2d35382ca4e692a47a4d232bf3892
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2021-10-25 18:04:42 -0300

    Comitando muita coisa mesmo!

--- core/home/models.py
+++ core/home/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Produto(models.Model):
     categoria = models.ManyToManyField('Categoria', related_name='produto') # o que esse related name faz?
-    name = models.CharField(max_length=200)
-    description = models.TextField(max_length=400)
-    image = models.ImageField(upload_to='img/products_images/') 
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    created_on = models.DateTimeField(auto_now_add=True)
-    is_vendido = models.BooleanField(default=False)
-    # fazer um campo para saber o valor que foi comprado
-    is_available = models.BooleanField(default=True)
+    name = models.CharField(max_length=200, verbose_name="Nome")
+    description = models.TextField(max_length=400, verbose_name="Descrição")
+    image = models.ImageField(upload_to='products_images/', verbose_name="Imagem") 
+    price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Preço")
+    created_on = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+    is_vendido = models.BooleanField(default=False, verbose_name="Vendido", help_text="O produto foi vendido?")
+    # fazer um campo para saber o valor que foi comprado == initial_price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Preço Inicial")
+    is_avaliable = models.BooleanField(default=True, verbose_name="Disponível", help_text="Está disponível?")
 
     objects = models.Manager()
   
