a7b82052b307677e9fe9175558e9ffd93a7075d0
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2021-10-25 19:18:35 -0300

    Adicionar arquivos para Heroku

--- core/core/settings/base.py
+++ core/core/settings/base.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,7 +127,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = BASE_DIR/ "staticfiles"
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
